3ddffcf3f5892fc1faddd19bddc61c894643c756
TenseTime <78992473+TenseTime@users.noreply.github.com>
2021-04-05 09:57:49 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paperObject = new Paper(100,600,10);
+	ground = new Ground(400,680,800,20);
+	leftside = new Dustbin(550, 620, 20,100);
+	bottom = new Dustbin(610,660,100,20);
+	rightSide = new Dustbin(670, 620, 20,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  paperObject.display();
+  ground.display();
+  leftside.display();
+  bottom.display();
+  rightSide.display();
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.appleForce(paperObject.body, paperObject.body.position,{x:15, y:-15});
+	}
+}
+
